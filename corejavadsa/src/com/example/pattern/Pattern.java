16ee98ba0836e278f605e8101f9bc52f618dfbc0
nirmal <nirmal.barik10@gmail.com>
2023-07-22 10:36:37 +0530

    pattern solid tangle

--- corejavadsa/src/com/example/pattern/Pattern.java
+++ corejavadsa/src/com/example/pattern/Pattern.java
@@ -1,36 +1,49 @@
 package com.example.pattern;
 
 public class Pattern {
-	//crea
+	// crea
 	public static void main(String[] args) {
-		//create pattern with solid rectangle 4*5 with star
-		//printSolidRectangle(4,5);
-		
-		//create pattern with hollow rectangle 4*5 with star
-		printHollowRectangle(4,5);
+		// create pattern with solid rectangle 4*5 with star
+		// printSolidRectangle(4,5);
+
+		// create pattern with hollow rectangle 4*5 with star
+		// printHollowRectangle(4,5);
+		// create pattern with solid trangle 4*5 with star
+		printSolidTrangle(4);
+	}
+
+	private static void printSolidTrangle(int row) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j <= i; j++) {
+
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
 	}
 
 	private static void printHollowRectangle(int row, int col) {
-		for(int i=0;i<row;i++) {
-			for(int j=0;j<col;j++) {
-				if(i==0 || j==0|| j==col-1 || i==row-1)
-				System.out.print("* ");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (i == 0 || j == 0 || j == col - 1 || i == row - 1)
+					System.out.print("* ");
 				else
-					System.out.print("  ");	
+					System.out.print("  ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 	private static void printSolidRectangle(int row, int col) {
-		for(int i=0;i<row;i++) {
-			for(int j=0;j<col;j++) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 }
