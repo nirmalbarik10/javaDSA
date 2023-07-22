1e496231c3ee45f08ebe10eba4e72815bb1d4e1a
nirmal <nirmal.barik10@gmail.com>
2023-07-22 20:13:37 +0530

    Bubble sort desc

--- corejavadsa/src/com/example/sort/SortArray.java
+++ corejavadsa/src/com/example/sort/SortArray.java
@@ -4,17 +4,32 @@ public class SortArray {
 
 	public static void main(String[] args) {
 		int[] arr = {7,8,3,1,2};
-		bubbleSort(arr);
+		//bubbleSort(arr);
+		bubbleSortdesc(arr);
 		for(int i:arr) {
 			System.out.print(i+" ");
 		}
 	}
-
+	//push biggest ele last
 	private static void bubbleSort(int[] arr) {
 		
 		for(int i=0;i<arr.length-1;i++) {
 			for(int j=0;j<arr.length-1-i;j++) {
 				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+				
+			}
+		}
+	}
+	
+private static void bubbleSortdesc(int[] arr) {
+		
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=0;j<arr.length-1-i;j++) {
+				if(arr[j]<arr[j+1]) {
 					int temp=arr[j];
 					arr[j]=arr[j+1];
 					arr[j+1]=temp;
