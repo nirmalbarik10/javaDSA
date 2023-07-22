3085dccc9d44a38e926a111fa7975e714d005569
nirmal <nirmal.barik10@gmail.com>
2023-07-22 17:13:34 +0530

    solidrhombus

--- corejavadsa/src/com/example/pattern/Pattern.java
+++ corejavadsa/src/com/example/pattern/Pattern.java
@@ -19,9 +19,27 @@ public class Pattern {
 		//printNumberPattern(5);
 		//printNumberPartenUpperTringle(5);
 		//printNumberInorder(4);
-		printnumber10F(4);
+		//printnumber10F(4);
+		PrintSolidRhombus(4);
+		
 	}
 	
+	private static void PrintSolidRhombus(int row) {
+		for(int i=row; i>=1; i--) {
+	           for(int j=1; j<=i; j++) {
+	        	  System.out.print(" ");
+	        	    
+	           }
+	           for(int j=1; j<=row; j++) {
+		        	  System.out.print("* ");
+		        	    
+		           }
+	           
+
+	           System.out.println();
+	       }
+	}
+
 	private static void printnumber10F(int row) {
 		for(int i=1; i<=row; i++) {
 	           for(int j=1; j<=i; j++) {
