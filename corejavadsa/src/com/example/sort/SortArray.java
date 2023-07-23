b65ba7ac8828ec5ee27332b7edf8eda99720c5f3
nirmal <nirmal.barik10@gmail.com>
2023-07-23 21:03:56 +0530

    sort array bubblesort,selection sort and merge sort

--- corejavadsa/src/com/example/sort/SortArray.java
+++ corejavadsa/src/com/example/sort/SortArray.java
@@ -6,7 +6,7 @@ public class SortArray {
 		int[] arr = {7,8,3,1,2};
 		//bubbleSort(arr);
 		//bubbleSortdesc(arr);
-		//selectionSort(arr);
+		selectionSort(arr);
 		insertionSort(arr);
 		
 		for(int i:arr) {
