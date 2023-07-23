be245ca20c7a02558e2bd157dea6bab524e91a54
nirmal <nirmal.barik10@gmail.com>
2023-07-23 22:48:53 +0530

    recursion

--- corejavadsa/src/com/example/recursion/Recursion.java
+++ corejavadsa/src/com/example/recursion/Recursion.java
@@ -7,8 +7,55 @@ public class Recursion {
 		//printNumber(5);
 		
 		//Print numbers from 1 to 5.
-		printNumberasc(1);
+		//printNumberasc(1);
+		
+		//Print the sum of first n natural numbers.
+		//int sum=printSum(5,0);
+		//System.out.println(sum);
+		
+		//Print factorial of a number n.
+		//int fact=printFact(5);
+		//System.out.println(fact);
+		
+		//Print the fibonacci sequence till nth term.
+		//printFibonacci(0,1,5);
+		
+		//Print x^n
+		int power=printPower(2,4);
+		System.out.println(power);
+	}
 
+	private static int printPower(int num, int n) {
+		if(n==0) {
+			return 1;
+		}
+		
+		return num*printPower( num,  n-1);
+	}
+
+	private static void printFibonacci(int a, int b, int n) {
+	if(n==0) {
+		return;
+	}
+		System.out.println(b);
+	printFibonacci( b, a+b,  n-1);
+		
+	}
+
+	private static int printFact(int num) {
+		if(num==1) {
+			return 1;
+		}		
+		return num*printFact(num-1);
+	}
+
+	private static int printSum(int num, int sum) {
+		if(num==0) {
+			return sum;
+		}
+		sum=sum+num;
+		return printSum( num-1,  sum);
+		
 	}
 
 	private static void printNumber(int num) {
