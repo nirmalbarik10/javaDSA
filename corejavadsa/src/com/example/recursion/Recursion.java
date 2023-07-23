dd841289401f8a91f3f19d5548fbc1cab79f39bb
nirmal <nirmal.barik10@gmail.com>
2023-07-23 21:41:24 +0530

    Print numbers from 1 to 5

--- corejavadsa/src/com/example/recursion/Recursion.java
+++ corejavadsa/src/com/example/recursion/Recursion.java
@@ -4,7 +4,10 @@ public class Recursion {
 
 	public static void main(String[] args) {
 		//Print numbers from 5 to 1
-		printNumber(5);
+		//printNumber(5);
+		
+		//Print numbers from 1 to 5.
+		printNumberasc(1);
 
 	}
 
@@ -14,6 +17,14 @@ public class Recursion {
 		}
 		System.out.print(num+" ");
 		printNumber(num-1);
+		
+	}
+	private static void printNumberasc(int num) {
+		if(num==6) {
+			return;
+		}
+		System.out.print(num+" ");
+		printNumberasc(num+1);
 		
 	}
 
